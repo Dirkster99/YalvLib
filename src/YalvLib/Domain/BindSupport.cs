bc6090421447803c7e59e57c1f1b9fa3432ac283
dirkster <Dirkster>
2013-04-15 17:33:20 +0200

    added missing BindSupport class file

---
+++ src/YalvLib/Domain/BindSupport.cs
@@ -0,0 +1,16 @@
+﻿namespace YalvLib.Domain
+{
+  using System.Windows;
+
+  public class BindSupport : DependencyObject
+  {
+    public static readonly DependencyProperty WidthProperty =
+        DependencyProperty.Register("Width", typeof(double), typeof(BindSupport), new UIPropertyMetadata(25.0));
+
+    public double Width
+    {
+      get { return (double)GetValue(WidthProperty); }
+      set { SetValue(WidthProperty, value); }
+    }
+  }
+}
