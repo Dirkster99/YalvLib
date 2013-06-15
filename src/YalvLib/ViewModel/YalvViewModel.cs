f8b55c2bd8eddf2be061005c27e2fac56249aebb
dirkster <Dirkster>
2013-06-15 05:44:00 +0200

    Conflict resolution

--- src/YalvLib/ViewModel/YalvViewModel.cs
+++ src/YalvLib/ViewModel/YalvViewModel.cs
@@ -1,34 +1,34 @@
 ﻿namespace YalvLib.ViewModel
 {
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+  using System;
+  using System.Collections.Generic;
+  using System.ComponentModel;
+  using System.Linq;
   using System.Windows;
   using YalvLib.Common;
   using YalvLib.Common.Interfaces;
-using YalvLib.Model;
-using YalvLib.Providers;
+  using YalvLib.Model;
+  using YalvLib.Providers;
   using YalvLib.Strings;
   using YalvLib.ViewModel.Process;
 
-    /// <summary>
-    /// Main ViewModel of Valv Lib control
-    /// </summary>
-    public class YalvViewModel : BindableObject
-    {
-        #region fields
+  /// <summary>
+  /// Main ViewModel of Valv Lib control
+  /// </summary>
+  public class YalvViewModel : BindableObject
+  {
+    #region fields
 
-        private readonly LogAnalysis _logAnalysis;
-        private readonly DisplayLogViewModel _logEntryRows;
-        private readonly ManageRepositoryViewModel _manageRepoViewModel;
-        private readonly ManageTextMarkersViewModel _manageTextMarkersViewModel;
- 
+    private readonly LogAnalysis _logAnalysis;
+    private readonly DisplayLogViewModel _logEntryRows;
+    private readonly ManageRepositoryViewModel _manageRepoViewModel;
+    private readonly ManageTextMarkersViewModel _manageTextMarkersViewModel;
+
     private LogFileLoader fileLoader = null;
 
-        private string _calculatedDelta;
+    private string _calculatedDelta;
 
-        #endregion fields
+    #endregion fields
 
     #region constructor
 
@@ -68,135 +68,102 @@ using YalvLib.Providers;
       if (this.fileLoader != null)
         this.fileLoader.Cancel();
 
-        return null;
+      return null;
     }
 
     #endregion constructor
 
     #region properties
-        /// <summary>
-        /// Defines files available extensions
-        /// </summary>
-        public static string FileExtensionDialogFilter
+    /// <summary>
+    /// Defines files available extensions
+    /// </summary>
+    public static string FileExtensionDialogFilter
+    {
+      get
+      {
+        return string.Format("*.log4j,*.log,*.txt,*.xml|*.log4j;*.log;*.txt;*.xml" +
+                             "|{0} (*.log4j)|*.log4j" +
+                             "|{1} (*.log)|*.log" +
+                             "|{2} (*.txt)|*.txt" +
+                             "|{3} (*.xml)|*.xml" +
+                             "|{4} (*.*)|*.*", "Log4j files",
+                             "Log files",
+                             "Text files",
+                             Resources.MainWindowVM_commandOpenFileExecute_XmlFilesCaption,
+                             Resources.MainWindowVM_commandOpenFileExecute_AllFilesCaption);
+      }
+    }
+
+
+    /// <summary>
+    /// Defines databases available extensions
+    /// </summary>
+    public static string DatabaseExtensionDialogFilter
+    {
+      get
+      {
+        return string.Format("{0}| *.db*" +
+                             "|{1} (*.*)|*.*", "Database files",
+                             Resources.MainWindowVM_commandOpenFileExecute_AllFilesCaption);
+      }
+    }
+
+    /// <summary>
+    /// Defines yalv files extension
+    /// </summary>
+    public static string LogAnalysisExtensionDialogFilter
+    {
+      get
+      {
+        return string.Format("{0}| *.yalv*" +
+                             "|{1} (*.*)|*.*", "LogAnalysis files",
+                             Resources.MainWindowVM_commandOpenFileExecute_AllFilesCaption);
+      }
+    }
+    #endregion constructor
+
+    #region properties
+
+    /// <summary>
+    /// Get property to determine the name of
+    /// the currently viewed log4net file or
+    /// empty string if there is no file being viewed at present.
+    /// </summary>
+    public List<string> FilePaths
+    {
+      get
+      {
+        var filePaths = new List<string>();
+        if (ManageRepositoriesViewModel != null)
         {
-            get
-            {
-                return string.Format("*.log4j,*.log,*.txt,*.xml|*.log4j;*.log;*.txt;*.xml" +
-                                     "|{0} (*.log4j)|*.log4j" +
-                                     "|{1} (*.log)|*.log" +
-                                     "|{2} (*.txt)|*.txt" +
-                                     "|{3} (*.xml)|*.xml" +
-                                     "|{4} (*.*)|*.*", "Log4j files",
-                                     "Log files",
-                                     "Text files",
-                                     Resources.MainWindowVM_commandOpenFileExecute_XmlFilesCaption,
-                                     Resources.MainWindowVM_commandOpenFileExecute_AllFilesCaption);
-            }
+          filePaths.AddRange(ManageRepositoriesViewModel.Repositories.Select(repo => repo.Repository.Path));
         }
+        return filePaths;
+      }
+    }
+
+    /// <summary>
+    /// Get a list of filter columns and data items (for display in a DataGridView)
+    /// </summary>
+    public DisplayLogViewModel LogEntryRows
+    {
+      get { return _logEntryRows; }
+    }
+
+    /// <summary>
+    /// Getter of the repositories managed view model
+    /// </summary>
+    public ManageRepositoryViewModel ManageRepositoriesViewModel
+    {
+      get { return _manageRepoViewModel; }
+    }
 
 
-        /// <summary>
-        /// Defines databases available extensions
-        /// </summary>
-        public static string DatabaseExtensionDialogFilter
-        {
-            get
-            {
-                return string.Format("{0}| *.db*" +
-                                     "|{1} (*.*)|*.*", "Database files",
-                                     Resources.MainWindowVM_commandOpenFileExecute_AllFilesCaption);
-            }
-        }
-
-        /// <summary>
-        /// Defines yalv files extension
-        /// </summary>
-        public static string LogAnalysisExtensionDialogFilter
-        {
-            get
-            {
-                return string.Format("{0}| *.yalv*" +
-                                     "|{1} (*.*)|*.*", "LogAnalysis files",
-                                     Resources.MainWindowVM_commandOpenFileExecute_AllFilesCaption);
-            }
-        }
-
-        /// <summary>
-        /// Standard constructor
-        /// </summary>
-        public YalvViewModel()
-        {
-            _manageTextMarkersViewModel = new ManageTextMarkersViewModel();
-            _manageRepoViewModel = new ManageRepositoryViewModel();
-            _manageRepoViewModel.ActiveChanged += ManageRepoViewModelOnPropertyChanged;
-
-            _logAnalysis = new LogAnalysis();
-            YalvRegistry.Instance.ActualWorkspace.CurrentAnalysis = _logAnalysis;
-
-            _logEntryRows = new DisplayLogViewModel(_manageTextMarkersViewModel);
-
-
-            CommandRefresh = new CommandRelay(CommandRefreshExecute, CommandRequiresDataCanExecute);
-            CommandDelete = new CommandRelay(LogEntryRows.CommandDeleteExecute,
-                                             LogEntryRows.CommandDeleteCanExecute);
-            FilterYalvView = new CommandRelay(CommandFilterYalvView, CommandRequiresDataCanExecute);
-
-            CommandUpdateTextMarkers = new CommandRelay(_manageTextMarkersViewModel.CommandUpdateTextMarkersExecute,
-                                                        _manageTextMarkersViewModel.CommandUpdateTextMarkersCanExecute);
-            CommandUpdateDelta = new CommandRelay(CommandUpdateDeltaExecute, CommandUpdateDeltaCanExecute);
-        }
-
-
-        private void ManageRepoViewModelOnPropertyChanged(object sender,
-                                                          PropertyChangedEventArgs propertyChangedEventArgs)
-        {
-            LoadFinishedEvent(true);
-        }
-
-        #endregion constructor
-
-        #region properties
-
-        /// <summary>
-        /// Get property to determine the name of
-        /// the currently viewed log4net file or
-        /// empty string if there is no file being viewed at present.
-        /// </summary>
-        public List<string> FilePaths
-        {
-            get
-            {
-                var filePaths = new List<string>();
-                if (ManageRepositoriesViewModel != null)
-                {
-                    filePaths.AddRange(ManageRepositoriesViewModel.Repositories.Select(repo => repo.Repository.Path));
-                }
-                return filePaths;
-            }
-        }
-
-        /// <summary>
-        /// Get a list of filter columns and data items (for display in a DataGridView)
-        /// </summary>
-        public DisplayLogViewModel LogEntryRows
-        {
-            get { return _logEntryRows; }
-        }
-
-        /// <summary>
-        /// Getter of the repositories managed view model
-        /// </summary>
-        public ManageRepositoryViewModel ManageRepositoriesViewModel
-        {
-            get { return _manageRepoViewModel; }
-        }
-
-
-        /// <summary>
-        /// Get the Display Text Marker instance
-        /// </summary>
-        public ManageTextMarkersViewModel ManageTextMarkersViewModel
-        {
+    /// <summary>
+    /// Get the Display Text Marker instance
+    /// </summary>
+    public ManageTextMarkersViewModel ManageTextMarkersViewModel
+    {
       get { return _manageTextMarkersViewModel; }
     }
 
@@ -208,51 +175,51 @@ using YalvLib.Providers;
     {
       get { return _calculatedDelta; }
       private set
-            {
+      {
         if (_calculatedDelta != value)
         {
           _calculatedDelta = value;
           NotifyPropertyChanged(() => CalculatedDelta);
-            }
         }
+      }
     }
 
-        /// <summary>
-        /// Get whether there are data items in the collection or not
-        /// (there may be no items to display if filter is applied but thats a different issue)
-        /// </summary>
-        public bool HasData
-        {
-            get { return (LogEntryRows != null && LogEntryRows.HasData); }
-        }
+    /// <summary>
+    /// Get whether there are data items in the collection or not
+    /// (there may be no items to display if filter is applied but thats a different issue)
+    /// </summary>
+    public bool HasData
+    {
+      get { return (LogEntryRows != null && LogEntryRows.HasData); }
+    }
 
-        #region Command
+    #region Command
 
-        /// <summary>
-        /// FilterYalvView command to switches a filtered view on or off
-        /// </summary>
-        public ICommandAncestor FilterYalvView { get; protected set; }
+    /// <summary>
+    /// FilterYalvView command to switches a filtered view on or off
+    /// </summary>
+    public ICommandAncestor FilterYalvView { get; protected set; }
 
-        /// <summary>
-        /// Refresh Command
-        /// </summary>
-        public ICommandAncestor CommandRefresh { get; protected set; }
+    /// <summary>
+    /// Refresh Command
+    /// </summary>
+    public ICommandAncestor CommandRefresh { get; protected set; }
 
-        /// <summary>
-        /// Delete Command
-        /// </summary>
-        public ICommandAncestor CommandDelete { get; protected set; }
+    /// <summary>
+    /// Delete Command
+    /// </summary>
+    public ICommandAncestor CommandDelete { get; protected set; }
 
 
-        /// <summary>
-        /// UpdateTextMarkers Command
-        /// </summary>
-        public ICommandAncestor CommandUpdateTextMarkers { get; protected set; }
+    /// <summary>
+    /// UpdateTextMarkers Command
+    /// </summary>
+    public ICommandAncestor CommandUpdateTextMarkers { get; protected set; }
 
-        /// <summary>
-        /// ChangeTextmarkers Command
-        /// </summary>
-        public ICommandAncestor CommandChangeTextMarkers { get; protected set; }
+    /// <summary>
+    /// ChangeTextmarkers Command
+    /// </summary>
+    public ICommandAncestor CommandChangeTextMarkers { get; protected set; }
 
 
     /// <summary>
@@ -261,11 +228,11 @@ using YalvLib.Providers;
     public ICommandAncestor CommandUpdateDelta { get; protected set; }
 
     public ICommandAncestor CommandCancelProcessing { get; protected set; }
-        #endregion Command
+    #endregion Command
 
-        #endregion properties
+    #endregion properties
 
-        #region methods
+    #region methods
     private void ManageRepoViewModelOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
     {
       LoadFinishedEvent(true);
@@ -298,79 +265,62 @@ using YalvLib.Providers;
       //// LoadFinishedEvent(true);
     }
 
-        /// <summary>
+    /// <summary>
     /// Method is executed when the background process finishes and returns here
     /// because it was cancelled or is done processing.
-        /// </summary>
+    /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
     private void fileFoader_loadResultEvent(object sender, LogFileLoader.ResultEvent e)
-        {
+    {
       this.fileLoader.loadResultEvent -= this.fileFoader_loadResultEvent;
       this.fileLoader = null;
 
-            LoadFinishedEvent(true);
-        }
+      LoadFinishedEvent(true);
+    }
 
-        /// <summary>
-        /// Load a database file
-        /// </summary>
-        /// <param name="path">file path</param>
-        public void LoadSqliteDatabase(string path)
-        {
+    /// <summary>
+    /// Load a database file
+    /// </summary>
+    /// <param name="path">file path</param>
+    public void LoadSqliteDatabase(string path)
+    {
       ManageRepositoriesViewModel.LoadFiles(new List<string> { path }, EntriesProviderType.Sqlite);
-            LoadFinishedEvent(true);
-        }
+      LoadFinishedEvent(true);
+    }
 
 
-        /// <summary>
-        /// Load an entire LogAnalysis session
-        /// </summary>
-        /// <param name="path">file path</param>
-        public void LoadLogAnalysisSession(string path)
-        {
+    /// <summary>
+    /// Load an entire LogAnalysis session
+    /// </summary>
+    /// <param name="path">file path</param>
+    public void LoadLogAnalysisSession(string path)
+    {
       ManageRepositoriesViewModel.LoadFiles(new List<string> { path }, EntriesProviderType.Yalv);
-            LoadFinishedEvent(true);
-        }
+      LoadFinishedEvent(true);
+    }
 
-        /// <summary>
-        /// Implementation of the Refresh command (reload data and apply filters)
-        /// </summary>
-        /// <param name="parameter"></param>
-        /// <returns></returns>
-        internal virtual object CommandRefreshExecute(object parameter)
-        {
-            LogEntryRows.CommandRefreshExecute(LoadFinishedEvent);
+    /// <summary>
+    /// Implementation of the Refresh command (reload data and apply filters)
+    /// </summary>
+    /// <param name="parameter"></param>
+    /// <returns></returns>
+    internal virtual object CommandRefreshExecute(object parameter)
+    {
+      LogEntryRows.CommandRefreshExecute(LoadFinishedEvent);
 
-            return null;
-        }
+      return null;
+    }
 
-        internal virtual bool CommandRequiresDataCanExecute(object parameter)
-        {
-            return HasData;
-        }
+    internal virtual bool CommandRequiresDataCanExecute(object parameter)
+    {
+      return HasData;
+    }
 
-
-        private bool CommandUpdateDeltaCanExecute(object obj)
-        {
-            return ((IEnumerable<LogEntryRowViewModel>) obj).Count() == 2;
-        }
-
-        private object CommandUpdateDeltaExecute(object arg)
-        {
-            var list = arg as IEnumerable<LogEntryRowViewModel>;
-            if (list != null)
-            {
-                // We sure has 2 and only 2 entries
-                var logEntryRowViewModels = list as List<LogEntryRowViewModel> ?? list.ToList();
-                LogEntryRowViewModel entry1 = logEntryRowViewModels.ElementAt(0);
-                LogEntryRowViewModel entry2 = logEntryRowViewModels.ElementAt(1);
-                CalculatedDelta = String.Concat("between GuId : ", entry1.Entry.GuId, " and GuId : ", entry2.Entry.GuId,
-                                                " = ",
-                                                GlobalHelper.GetTimeDelta(entry1.Entry.TimeStamp, entry2.Entry.TimeStamp));
-            }
-            return null;
-        }
+    private bool CommandUpdateDeltaCanExecute(object obj)
+    {
+      return ((IEnumerable<LogEntryRowViewModel>)obj).Count() == 2;
+    }
 
     private object CommandUpdateDeltaExecute(object arg)
     {
@@ -388,43 +338,43 @@ using YalvLib.Providers;
       return null;
     }
 
-        /// <summary>
-        /// Default command method for applying column filters.
-        /// </summary>
-        /// <param name="parameter"></param>
-        /// <returns></returns>
-        protected virtual object CommandFilterYalvView(object parameter)
-        {
-            if (_logEntryRows != null)
-            {
-                _logEntryRows.ApplyFilter();
-            }
+    /// <summary>
+    /// Default command method for applying column filters.
+    /// </summary>
+    /// <param name="parameter"></param>
+    /// <returns></returns>
+    protected virtual object CommandFilterYalvView(object parameter)
+    {
+      if (_logEntryRows != null)
+      {
+        _logEntryRows.ApplyFilter();
+      }
 
-            return null;
-        }
+      return null;
+    }
 
-        /// <summary>
-        /// This is a callback method that is always called when
-        /// the internal load process is finished
-        /// (even when it failed to finish after initialization).
-        /// </summary>
-        /// <param name="loadWasSuccessful"></param>
-        private void LoadFinishedEvent(bool loadWasSuccessful)
-        {
+    /// <summary>
+    /// This is a callback method that is always called when
+    /// the internal load process is finished
+    /// (even when it failed to finish after initialization).
+    /// </summary>
+    /// <param name="loadWasSuccessful"></param>
+    private void LoadFinishedEvent(bool loadWasSuccessful)
+    {
       this.ManageRepositoriesViewModel.IsLoading = false;
       LogEntryRows.SetEntries(ManageRepositoriesViewModel.Repositories.ToList());
       RefreshCommandsCanExecute();
-        }
-
-        private void RefreshCommandsCanExecute()
-        {
-            CommandRefresh.CanExecute(null);
-            CommandDelete.CanExecute(null);
-            FilterYalvView.CanExecute(null);
-
-            RaisePropertyChanged("HasData");
-        }
-
-        #endregion methods
     }
+
+    private void RefreshCommandsCanExecute()
+    {
+      CommandRefresh.CanExecute(null);
+      CommandDelete.CanExecute(null);
+      FilterYalvView.CanExecute(null);
+
+      RaisePropertyChanged("HasData");
+    }
+
+    #endregion methods
+  }
 }
