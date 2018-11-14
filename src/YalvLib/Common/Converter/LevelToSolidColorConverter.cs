120ca62412c7e34e969e386d1783addf794d83ee
Dirkster99 <Dirk.Bahle@gmx.de>
2018-11-14 01:23:33 +0100

    Added resource keys and code cleaning around theming items

--- src/YalvLib/Common/Converter/LevelToSolidColorConverter.cs
+++ src/YalvLib/Common/Converter/LevelToSolidColorConverter.cs
@@ -1,78 +1,158 @@
 ﻿namespace YalvLib.Common.Converters
 {
-  using System;
-  using System.Windows;
-  using System.Windows.Data;
-  using System.Windows.Media;
-  using YalvLib.Model;
-
-  /// <summary>
-  /// Convert a log4net message level (Info, Warn, Debug) into a color code.
-  /// </summary>
-  [ValueConversion(typeof(LevelIndex), typeof(SolidColorBrush))]
-  public class LevelToSolidColorConverter : IValueConverter
-  {
-    private SolidColorBrush debugColor = Application.Current.FindResource("DebugLevelColor") as SolidColorBrush;
-    private SolidColorBrush infoColor  = Application.Current.FindResource("InfoLevelColor") as SolidColorBrush;
-    private SolidColorBrush warnColor  = Application.Current.FindResource("WarnLevelColor") as SolidColorBrush;
-    private SolidColorBrush errorColor = Application.Current.FindResource("ErrorLevelColor") as SolidColorBrush;
-    private SolidColorBrush fatalColor = Application.Current.FindResource("FatalLevelColor") as SolidColorBrush;
+    using System;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Media;
+    using YalvLib.Model;
 
     /// <summary>
-    /// Convert <seealso cref="LevelIndex"/> enum value into a color value based on a corresponding resource.
+    /// Convert a log4net message level (Info, Warn, Debug) into a color code.
     /// </summary>
-    /// <param name="value"></param>
-    /// <param name="targetType"></param>
-    /// <param name="parameter"></param>
-    /// <param name="culture"></param>
-    /// <returns></returns>
-    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    [ValueConversion(typeof(LevelIndex), typeof(SolidColorBrush))]
+    public class LevelToSolidColorConverter : DependencyObject, IValueConverter
     {
-      if (null == value)
-        return Brushes.Transparent;
+        #region fields
+        /// <summary>
+        /// Backing store of the debug color level dependency property.
+        /// </summary>
+        public static readonly DependencyProperty DebugColorProperty =
+            DependencyProperty.Register("DebugColor", typeof(SolidColorBrush),
+                typeof(LevelToSolidColorConverter), new PropertyMetadata(null));
 
-      if ((value is LevelIndex) == false)
-        return Brushes.Transparent;
+        /// <summary>
+        /// Backing store of the information color level dependency property.
+        /// </summary>
+        public static readonly DependencyProperty InfoColorProperty =
+            DependencyProperty.Register("InfoColor", typeof(SolidColorBrush),
+                typeof(LevelToSolidColorConverter), new PropertyMetadata(null));
 
-      LevelIndex levelIndex = (LevelIndex)value;
+        /// <summary>
+        /// Backing store of the warn color level dependency property.
+        /// </summary>
+        public static readonly DependencyProperty WarnColorProperty =
+            DependencyProperty.Register("WarnColor", typeof(SolidColorBrush),
+                typeof(LevelToSolidColorConverter), new PropertyMetadata(null));
 
-      switch (levelIndex)
-      {
-        case LevelIndex.NONE:
-          return Brushes.Transparent;
+        /// <summary>
+        /// Backing store of the error color level dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ErrorColorProperty =
+            DependencyProperty.Register("ErrorColor", typeof(SolidColorBrush),
+                typeof(LevelToSolidColorConverter), new PropertyMetadata(null));
 
-        case LevelIndex.DEBUG:
-          return this.debugColor ?? Brushes.Transparent;
+        /// <summary>
+        /// Backing store of the fatal color level dependency property.
+        /// </summary>
+        public static readonly DependencyProperty FatalColorProperty =
+            DependencyProperty.Register("FatalColor", typeof(SolidColorBrush),
+                typeof(LevelToSolidColorConverter), new PropertyMetadata(null));
+        #endregion fields
 
-        case LevelIndex.INFO:
-          return this.infoColor ?? Brushes.Transparent;
+        #region properties
+        /// <summary>
+        /// Gets/sets dependency property for the debug level background color.
+        /// </summary>
+        public SolidColorBrush DebugColor
+        {
+            get { return (SolidColorBrush)GetValue(DebugColorProperty); }
+            set { SetValue(DebugColorProperty, value); }
+        }
 
-        case LevelIndex.WARN:
-          return this.warnColor ?? Brushes.Transparent;
+        /// <summary>
+        /// Gets/sets dependency property for the info level background color.
+        /// </summary>
+        public SolidColorBrush InfoColor
+        {
+            get { return (SolidColorBrush)GetValue(InfoColorProperty); }
+            set { SetValue(InfoColorProperty, value); }
+        }
 
-        case LevelIndex.ERROR:
-          return this.errorColor ?? Brushes.Transparent;
+        /// <summary>
+        /// Gets/sets dependency property for the warning level background color.
+        /// </summary>
+        public SolidColorBrush WarnColor
+        {
+            get { return (SolidColorBrush)GetValue(WarnColorProperty); }
+            set { SetValue(WarnColorProperty, value); }
+        }
 
-        case LevelIndex.FATAL:
-          return this.fatalColor ?? Brushes.Transparent;
+        /// <summary>
+        /// Gets/sets dependency property for the error level background color.
+        /// </summary>
+        public SolidColorBrush ErrorColor
+        {
+            get { return (SolidColorBrush)GetValue(ErrorColorProperty); }
+            set { SetValue(ErrorColorProperty, value); }
+        }
 
-        default:
-          throw new NotImplementedException(levelIndex.ToString());
-      }
+        /// <summary>
+        /// Gets/sets dependency property for the fatal level background color.
+        /// </summary>
+        public SolidColorBrush FatalColor
+        {
+            get { return (SolidColorBrush)GetValue(FatalColorProperty); }
+            set { SetValue(FatalColorProperty, value); }
+        }
+        #endregion properties
+
+        #region methods
+        /// <summary>
+        /// Convert <seealso cref="LevelIndex"/> enum value into a color value based on a corresponding resource.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (null == value)
+                return Brushes.Transparent;
+
+            if ((value is LevelIndex) == false)
+                return Brushes.Transparent;
+
+            LevelIndex levelIndex = (LevelIndex)value;
+
+            switch (levelIndex)
+            {
+                case LevelIndex.NONE:
+                    return Brushes.Transparent;
+
+                case LevelIndex.DEBUG:
+                    return this.DebugColor ?? Brushes.Transparent;
+
+                case LevelIndex.INFO:
+                    return this.InfoColor ?? Brushes.Transparent;
+
+                case LevelIndex.WARN:
+                    return this.WarnColor ?? Brushes.Transparent;
+
+                case LevelIndex.ERROR:
+                    return this.ErrorColor ?? Brushes.Transparent;
+
+                case LevelIndex.FATAL:
+                    return this.FatalColor ?? Brushes.Transparent;
+
+                default:
+                    throw new NotImplementedException(levelIndex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Conversion back from color code into <seealso cref="LevelIndex"/> enum value
+        /// is not supported (not implenented exception is thrown).
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException("Conversion from color code to LevelIndex enum value is not supported.");
+        }
+        #endregion methods
     }
-
-    /// <summary>
-    /// Conversion back from color code into <seealso cref="LevelIndex"/> enum value
-    /// is not supported (not implenented exception is thrown).
-    /// </summary>
-    /// <param name="value"></param>
-    /// <param name="targetType"></param>
-    /// <param name="parameter"></param>
-    /// <param name="culture"></param>
-    /// <returns></returns>
-    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-    {
-      throw new NotImplementedException("Conversion from color code to LevelIndex enum value is not supported.");
-    }
-  }
 }
--- src/YalvLib/Themes/ResourceKeys.cs
+++ src/YalvLib/Themes/ResourceKeys.cs
@@ -27,7 +27,6 @@
         /// </summary>
         public static readonly ComponentResourceKey ControlNormalBackgroundKey = new ComponentResourceKey(typeof(ResourceKeys), "ControlNormalBackgroundKey");
 
-
         /// <summary>
         /// Gets a the applicable foreground Brush key that should be used for coloring text.
         /// </summary>
@@ -43,9 +42,91 @@
         /// </summary>
         public static readonly ComponentResourceKey ControlBorderBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "ControlBorderBrushKey");
 
+        #region DataGridHeader Keys
         /// <summary>
-        /// Gets the Brush key of the border color of a control.
+        /// Gets the Brush key of the border color
+        /// of the vertical Data Grid Header line between each header item.
         /// </summary>
         public static readonly ComponentResourceKey DataGridHeaderBorderBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "DataGridHeaderBorderBrushKey");
+
+        /// <summary>
+        /// Gets the background color of the datagrid header row.
+        /// </summary>
+        public static readonly ComponentResourceKey DataGridHeaderBackgroundBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "DataGridHeaderBackgroundBrushKey");
+
+        /// <summary>
+        /// Gets the foreground color of the datagrid header row.
+        /// </summary>
+        public static readonly ComponentResourceKey DataGridHeaderForegroundBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "DataGridHeaderForegroundBrushKey");
+
+        /// <summary>
+        /// Gets the foreground color of the sort arrow display in the datagrid header row.
+        /// </summary>
+        public static readonly ComponentResourceKey DataGridHeaderSortArrowForegroundBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "DataGridHeaderSortArrowForegroundBrushKey");
+
+        /// <summary>
+        /// Gets the font size of the text displayed in the DataGrid header row.
+        /// </summary>
+        public static readonly ComponentResourceKey DataGridHeaderFontSize = new ComponentResourceKey(typeof(ResourceKeys), "DataGridHeaderFontSize");
+        #endregion DataGridHeader Keys
+
+        /// <summary>
+        /// Gets the Brush key of the border color
+        /// between each column in the data display area of the DataGrid.
+        /// </summary>
+        public static readonly ComponentResourceKey VerticalGridLinesBrush = new ComponentResourceKey(typeof(ResourceKeys), "VerticalGridLinesBrush");
+
+        /// <summary>
+        /// Gets the Brush key of the border color
+        /// between each row in the data display area of the DataGrid.
+        /// </summary>
+        public static readonly ComponentResourceKey HorizontalGridLinesBrush = new ComponentResourceKey(typeof(ResourceKeys), "HorizontalGridLinesBrush");
+
+        /// <summary>
+        /// Gets the foreground brush key of a datagrid cell that is selected and focused.
+        /// </summary>
+        public static readonly ComponentResourceKey SelectedFocusedCellForegroundBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "SelectedFocusedCellForegroundBrushKey");
+
+        /// <summary>
+        /// Gets the background brush key of a datagrid cell that is selected and focused.
+        /// </summary>
+        public static readonly ComponentResourceKey SelectedFocusedCellBackgroundBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "SelectedFocusedCellBackgroundBrushKey");
+
+        /// <summary>
+        /// Gets the foreground brush key of a datagrid cell that is selected but not focused.
+        /// </summary>
+        public static readonly ComponentResourceKey SelectedCellForegroundBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "SelectedCellForegroundBrushKey");
+
+        /// <summary>
+        /// Gets the background brush key of a datagrid cell that is selected but not focused.
+        /// </summary>
+        public static readonly ComponentResourceKey SelectedCellBackgroundBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "SelectedCellBackgroundBrushKey");
+
+        #region LogLevel BrushKeys
+        /// <summary>
+        /// Gets the background brush key for the data row that represents a debug level log entry.
+        /// </summary>
+        public static readonly ComponentResourceKey DebugLevelBackgroundBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "DebugLevelBackgroundBrushKey");
+
+        /// <summary>
+        /// Gets the background brush key for the data row that represents a information level log entry.
+        /// </summary>
+        public static readonly ComponentResourceKey InfoLevelBackgroundBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "InfoLevelBackgroundBrushKey");
+
+        /// <summary>
+        /// Gets the background brush key for the data row that represents a warning level log entry.
+        /// </summary>
+        public static readonly ComponentResourceKey WarnLevelBackgroundBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "WarnLevelBackgroundBrushKey");
+
+        /// <summary>
+        /// Gets the background brush key for the data row that represents a error level log entry.
+        /// </summary>
+        public static readonly ComponentResourceKey ErrorLevelBackgroundBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "ErrorLevelBackgroundBrushKey");
+
+        /// <summary>
+        /// Gets the background brush key for the data row that represents a fatal level log entry.
+        /// </summary>
+        public static readonly ComponentResourceKey FatalLevelBackgroundBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "FatalLevelBackgroundBrushKey");
+        #endregion LogLevel BrushKeys
     }
 }
